74d80ac06e9b84a71f8794b2522148e6998c2e7b
emile <emile@EJDER>
2023-09-27 18:36:14 +0200

    Add project files.

---
+++ SettingStuffUpp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SettingStuffUpp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
